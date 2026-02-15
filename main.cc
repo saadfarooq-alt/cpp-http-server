d4e321b3241b2b4a730e0e8b4bd6c5be71f5a818
Sa'ad Farooq <saadfarooq7498@gmail.com>
2026-02-15 17:18:41 -0500

    Integrate SFML for graphical client interaction
    Replaced socket communication with SFML graphics for client interaction.

--- main.cc
+++ main.cc
@@ -1,17 +1,12 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <cstring>
-#include <cstdlib>
-#include <ctime>
+#include <iostream>
 
 int main() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd < 0) {
-        std::cerr << "Failed to create socket\n";
-        return 1;
-    }
+    if (server_fd < 0) { std::cerr << "Socket error\n"; return 1; }
 
     sockaddr_in address;
     address.sin_family = AF_INET;
@@ -19,50 +14,38 @@ int main() {
     address.sin_port = htons(8080);
 
     if (::bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        std::cerr << "Bind failed\n";
-        return 1;
+        std::cerr << "Bind failed\n"; return 1;
     }
-
-    if (listen(server_fd, 5) < 0) {
-        std::cerr << "Listen failed\n";
-        return 1;
-    }
+    if (listen(server_fd, 5) < 0) { std::cerr << "Listen failed\n"; return 1; }
 
     std::cout << "Server listening on port 8080...\n";
 
     int addrlen = sizeof(address);
     int client_fd = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
-    if (client_fd < 0) {
-        std::cerr << "Accept failed\n";
-        return 1;
+    if (client_fd < 0) { std::cerr << "Accept failed\n"; return 1; }
+
+    std::cout << "Client connected! Opening a new window...\n";
+
+    // --- SFML window ---
+    sf::RenderWindow window(sf::VideoMode(400, 200), "New Window");
+    sf::Font font;
+    if (!font.loadFromFile("/System/Library/Fonts/SFNSDisplay.ttf")) {
+        std::cerr << "Font load failed\n";
     }
 
-    std::cout << "Client connected!\n";
+    sf::Text text("Hello! Client connected!", font, 20);
+    text.setFillColor(sf::Color::White);
+    text.setPosition(20, 80);
 
-    // Seed random number for the game
-    std::srand(std::time(nullptr));
-    int secret = std::rand() % 100 + 1;
-
-    char buffer[1024] = {0};
-    const char* msg = "Guess a number between 1 and 100:\n";
-    send(client_fd, msg, strlen(msg), 0);
-
-    while (true) {
-        int valread = read(client_fd, buffer, sizeof(buffer) - 1);
-        if (valread <= 0) break; // client disconnected
-
-        buffer[valread] = '\0'; // null-terminate string
-        int guess = atoi(buffer);
-
-        if (guess < secret) send(client_fd, "Too low!\n", 9, 0);
-        else if (guess > secret) send(client_fd, "Too high!\n", 10, 0);
-        else {
-            send(client_fd, "Correct! You win!\n", 18, 0);
-            secret = std::rand() % 100 + 1; // reset game
-            send(client_fd, "Guess a number between 1 and 100:\n", 35, 0);
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
         }
-
-        memset(buffer, 0, sizeof(buffer));
+        window.clear(sf::Color::Black);
+        window.draw(text);
+        window.display();
     }
 
     close(client_fd);
