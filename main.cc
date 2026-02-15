0932bac81853b3a1642ce87d145e36d862788469
Sa'ad Farooq <saadfarooq7498@gmail.com>
2026-02-15 17:09:26 -0500

    Refactor socket server to use std::cout and std::cerr

--- main.cc
+++ main.cc
@@ -4,61 +4,45 @@
 #include <unistd.h>
 #include <string.h>
 
-using namespace std;
-
 int main() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
-        cerr << "Failed to create socket" << endl;
+        std::cerr << "Failed to create socket" << std::endl;
         return 1;
     }
 
-    cout << "Socket created successfully" << endl;
+    std::cout << "Socket created successfully" << std::endl;
 
     sockaddr_in address;
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY; // Listen on all interfaces
+    address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
 
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        cerr << "Bind failed" << endl;
+    // Use global ::bind to avoid conflict with std::bind
+    if (::bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        std::cerr << "Bind failed" << std::endl;
         return 1;
     }
 
     if (listen(server_fd, 5) < 0) {
-        cerr << "Listen failed" << endl;
+        std::cerr << "Listen failed" << std::endl;
         return 1;
     }
 
-    cout << "Server listening on port 8080..." << endl;
+    std::cout << "Server listening on port 8080..." << std::endl;
 
     int addrlen = sizeof(address);
     int client_fd = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
     if (client_fd < 0) {
-        cerr << "Accept failed" << endl;
+        std::cerr << "Accept failed" << std::endl;
         return 1;
     }
 
-    cout << "Client connected!" << endl;
+    std::cout << "Client connected!" << std::endl;
 
-    // Simple game: guess a number
-    int secret = rand() % 100 + 1;
     char buffer[1024] = {0};
-    send(client_fd, "Guess a number 1-100:\n", 23, 0);
-
-    while (true) {
-        int valread = read(client_fd, buffer, 1024);
-        if (valread <= 0) break;
-
-        int guess = atoi(buffer);
-        if (guess < secret) send(client_fd, "Too low!\n", 9, 0);
-        else if (guess > secret) send(client_fd, "Too high!\n", 10, 0);
-        else {
-            send(client_fd, "Correct! You win!\n", 18, 0);
-            secret = rand() % 100 + 1; // Reset game
-        }
-        memset(buffer, 0, sizeof(buffer));
-    }
+    const char* msg = "Hello from server!\n";
+    send(client_fd, msg, strlen(msg), 0);
 
     close(client_fd);
     close(server_fd);
